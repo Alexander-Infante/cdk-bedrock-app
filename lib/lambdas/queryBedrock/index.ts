e4223bc4ea28fdfbd63fc1492f74d7cda1a36161
Alexander-Infante <alexinfante17@gmail.com>
2025-02-08 10:32:49 -0800

    Minor prompt change

--- lib/lambdas/queryBedrock/index.ts
+++ lib/lambdas/queryBedrock/index.ts
@@ -25,7 +25,7 @@ export const handler = async (event: any): Promise<any> => {
      * This adjusting of the prompt is called "Prompt Engineering"
      * https://docs.anthropic.com/en/docs/build-with-claude/prompt-engineering/overview
      */
-    const prompt = `Summarize the following data of kubernetes clusters: ${JSON.stringify(
+    const prompt = `Summarize the following data of kubernetes clusters information: ${JSON.stringify(
       requestBody.inputData
     )}`;
 
